895993f79e0eca38458f5e9278acf1cd6b271570
lc <liuchong@fontdo.com>
2025-02-10 17:25:46 +0800

    fix ppp packet parser

--- l2tp/ppp.go
+++ l2tp/ppp.go
@@ -485,6 +485,6 @@ func parsePPPBuffer(b []byte, p *pppPayload) (err error) {
 	p.code = b[0]
 	p.identifier = b[1]
 	p.length = binary.BigEndian.Uint16(b[2:4])
-	p.data = b[4:]
+	p.data = b[4:p.length]
 	return nil
 }
